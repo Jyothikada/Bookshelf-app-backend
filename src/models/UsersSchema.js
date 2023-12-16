63fa5f7a599596d751c0753d56441ed53451d18e
Jyothikada <128328310+Jyothikada@users.noreply.github.com>
2023-12-17 02:26:01 +0530

    Add files via upload

---
+++ src/models/UsersSchema.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    savedBooks: [{type: mongoose.Schema.Types.ObjectId, ref: "book"}]
+});
+
+
+export const UserModel = mongoose.model("users", UserSchema);
---
+++ src/routes/login.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { UserModel } from "../models/UsersSchema.js"
+
+const router = express.Router();
+
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await UserModel.findOne({ username });
+  
+    if (!user) {
+      return res.status(401).json({ message: "Username or Password is Incorrect" });
+    }
+  
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Username or Password is Incorrect" });
+    }
+  
+    const token = jwt.sign({ id: user._id }, "secret");
+    res.json({ token, userID: user._id });
+  });
+
+
+  export { router as loginRouter };
